bd385efe6be84ff984d5616249f0dd003b69c84b
Luke Bassett <bassettl@wit.edu>
2023-05-30 09:44:09 -0400

    Added courses db and initial front end integration

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+import sqlite3
 
-
+# creates a sql database called courses.db on the local dir
+connection = sqlite3.connect("courses.db")
 
 class leopardWeb(App):
     def build(self):
         return Label(text= "La website")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     leopardWeb().run()
 
