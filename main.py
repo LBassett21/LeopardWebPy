23e3ae48fc3bc5b1abd99fc294f638e63447e624
Jack Krupienski <krupienskij@wit.edu>
2023-05-30 09:12:17 -0400

    Added a text box for initial GUI things

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-from user import user
-from student import student
-from instructor import instructor
-from admin import admin
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
 
-engStud = student("Luke", "Bassett", "W001")
-print("Info for user: ", engStud.getID(), " - ", engStud.getFirstName(), engStud.getLastName())
-engStud.addDropCourse("1390141", True)
-engStud.addDropCourse("1390141", False)
-engStud.printSchedule()
-engStud.setID("W002")
-print("Info for user: ", engStud.getID(), " - ", engStud.getFirstName(), engStud.getLastName())
 
-prof = instructor("Marisha", "Rawlins", "W003")
-print("Info for user: ", prof.getID(), " - ", prof.getFirstName(), prof.getLastName())
-prof.printClassList()
-prof.searchCourse("1390141")
-prof.printSchedule()
 
-assistant = admin("Cindy", "Rosner", "W004")
-print("Info for user: ", assistant.getID(), " - ", assistant.getFirstName(), assistant.getLastName())
-assistant.addCourse("Applied Programming Concepts", "Cool programming class", "1390141")
-assistant.addRemoveUser(True, "W001")
-assistant.addRemoveUser(False, "W001")
-assistant.addRemoveUserCourse(True, "W002", "1390131")
-assistant.addRemoveUserCourse(False, "W002", "1390141")
-assistant.removeCourse("1390141")
-assistant.printRoster("1390141")
+class leopardWeb(App):
+    def build(self):
+        return Label(text= "La website")
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    leopardWeb().run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
