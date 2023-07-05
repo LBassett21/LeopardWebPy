5b5757ea4199999b2b7298690fee67e465b4ce12
Luke Bassett <bassettl@wit.edu>
2023-07-05 15:19:18 -0400

    Added to database, fixed any merge conflicts.

--- main.py
+++ main.py
@@ -3,64 +3,15 @@ from tkinter import Label
 from tkinter import *
 from PIL import ImageTk,Image
 import webview
+import database
 import sqlite3
 
 # creates sql databases called courses.db and userinfo.db on the local dir
 coursesdb = sqlite3.connect("courses.db")
 userinfodb = sqlite3.connect("userinfo.db")
 
-
-
 coursesCur = coursesdb.cursor()
 userinfoCur=userinfodb.cursor()
-# # creates a table with the following categories:
-# # CRN (primary key) | subject | course number | section number | title | term | type | credit hours | start time | end time
-# # coursesCur.execute("CREATE TABLE courses (crn INTEGER PRIMARY KEY, subject TEXT ,coursenum INTEGER, sectionnum INTEGER, title TEXT, term TEXT, type TEXT, credithr INTEGER, starttime INTEGER, endtime INTEGER)")
-# #coursesCur.execute("INSERT INTO courses VALUES (33950, 'ELEC', 3225, 01, 'APPLIED PROGRAMMING CONCEPTS', 'Summer 2023', 'Lecture (LEC)', 3, 0800, 0950)")
-#
-# # id (primary key) | username | password | user type
-# #userinfoCur.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, type TEXT)")
-#
-# class app:
-#     def __init__(self, master):
-#         self.master = master
-#         self.master.geometry("200x200")
-#         self.login()
-#
-#
-#
-#
-#
-#
-#
-# setID = 403388
-# setUserName = "Admin"
-# setPassword = "Password"
-# setType = "Admin"
-#
-# #userinfoCur.execute("INSERT INTO users (id, username, password, type) VALUES (?, ?, ?, ?)",(setID, setUserName, setPassword, setType))
-# #userinfodb.commit()
-#
-# # variables to be added to the table, we can change these variables later, just to test if one course works
-# setCRN = 33950
-# setSubject = "ELEC"
-# setCourseNum = 3225
-# setSectNum = 1
-# setTitle = "APPLIED PROGRAMMING CONCEPTS"
-# setTerm = "Summer 2023"
-# setType = "Lecture (LEC)"
-# setCredHr = 3
-# setStartTime = 800
-# setEndTime = 950
-#
-# # adds each of the variables to the database
-# # coursesCur.execute("INSERT INTO courses (crn, subject, coursenum, sectionnum, title, term, type, credithr, starttime, endtime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (setCRN, setSubject, setCourseNum, setSectNum, setTitle, setTerm, setType, setCredHr, setStartTime, setEndTime))
-# # coursesdb.commit()
-#
-# # Test printout of data in courses database
-# #testPrint = coursesCur.execute("SELECT crn, subject, coursenum, sectionnum, title, term, type, credithr FROM courses").fetchall()
-# #print(testPrint)
-#
 
 #
 #
@@ -291,5 +242,3 @@ def main():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-
